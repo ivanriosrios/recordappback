d63b42c2e4906ca4fd5eab93f4a4b01169a94708
Ivan Rios <ivanargemiroriosrios@gmail.com>
2026-03-18 18:29:06 -0500

    feat: add Appointment, BusinessSchedule, and ConversationState models to the exports

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -6,6 +6,9 @@ from app.models.template import Template
 from app.models.reminder_log import ReminderLog
 from app.models.service_log import ServiceLog
 from app.models.notification import Notification, NotificationType
+from app.models.appointment import Appointment, AppointmentStatus, AppointmentShift
+from app.models.business_schedule import BusinessSchedule
+from app.models.conversation_state import ConversationState
 
 __all__ = [
     "Business",
@@ -17,4 +20,9 @@ __all__ = [
     "ServiceLog",
     "Notification",
     "NotificationType",
+    "Appointment",
+    "AppointmentStatus",
+    "AppointmentShift",
+    "BusinessSchedule",
+    "ConversationState",
 ]
